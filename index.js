bbb28c97f3416826da7d25c60a24e4c1f667c317
JAILSON LOPES FARIAS <pigrupo030@gmail.com>
2022-05-15 11:04:07 -0300

    criação do projeto - back end - node.js com express - criação das rotas

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express"); //importando o express
+const app = express(); //iniciando o express
+
+app.set('view engine','ejs'); //dizendo para o express usar o EJS como View engine
+
+app.get("/home",function(req,res){
+    res.render("home");
+});
+
+app.get("/cadastro",function(req,res){
+    res.render("cadastro");
+})
+
+app.get("/mural",function(req,res){
+    res.render("mural");
+})
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
+
+app.listen(30,function(erro){
+    if(erro){
+        console.log("Ocorreu um erro!");
+    }else{
+        console.log("servidor iniciado com sucesso!");
+    }
+
+})
